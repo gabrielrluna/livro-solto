f1af5227ccc175a2f11ee34e277c0104f2c31493
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-11-25 10:19:56 -0300

    sobre-básico

---
+++ src/screens/Sobre.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Sobre = () => {
+  return (
+    <View>
+      <Text>Sobre</Text>
+    </View>
+  );
+};
+
+export default Sobre;
+
+const styles = StyleSheet.create({});
