b8821e90ba41dec9bef68ec11d0042c62beafa8f
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-11-25 10:22:12 -0300

    favoritos-básico

---
+++ src/screens/Favoritos.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Favoritos = () => {
+  return (
+    <View>
+      <Text>Favoritos</Text>
+    </View>
+  );
+};
+
+export default Favoritos;
+
+const styles = StyleSheet.create({});
