f54bfc74a005f70a7ec8ed7619d21f05cd25b52f
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-11-25 10:23:41 -0300

    anunciar-básico

---
+++ src/screens/AnunciarLivro.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AnunciarLivro = () => {
+  return (
+    <View>
+      <Text>AnunciarLivro</Text>
+    </View>
+  );
+};
+
+export default AnunciarLivro;
+
+const styles = StyleSheet.create({});
