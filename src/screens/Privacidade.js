f1b5c9172616a578f3f0265093933bab6c1324c5
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2023-01-30 11:01:28 -0300

    Sobre e privacidade
    Telas com os textos

--- src/screens/Privacidade.js
+++ src/screens/Privacidade.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
 
 const Privacidade = () => {
   return (
-    <View>
-      <Text>Privacidade</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Termos de Privacidade</Text>
+      <View style={styles.viewTexto}>
+        <Text style={styles.paragraph}>
+          A livro solto se compromete com a segurança dos dados de seus usuários
+          com padrões rigorosos.
+        </Text>
+        <Text style={styles.paragraph}>
+          A livro solto se dispõe no direito de solicitar dados de seus usuários
+          para efetuação do cadastro que será movimentado na utilização do site.
+        </Text>
+        <Text style={styles.paragraph}>
+          A livro solto se compromete a não utilizar os dados fornecidos pelos
+          usuários, para realizar envios de propagandas, spam, ou ofertas de
+          produtos.
+        </Text>
+        <Text style={styles.paragraph}>
+          Todo e qualquer dado fornecido pelo usuário será utilizado apenas com
+          intuito de utilização site e para a identificação do mesmo.
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Privacidade;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 18,
+    alignItems: "center",
+    fontFamily: "roboto",
+  },
+  titulo: {
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "roboto",
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  viewTexto: { marginVertical: 8 },
+  paragraph: {
+    textAlign: "justify",
+    marginVertical: 8,
+    fontSize: 18,
+    fontFamily: "roboto",
+  },
+});
--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useFonts } from "expo-font";
 
 const Sobre = () => {
   return (
-    <View>
-      <Text>Sobre</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>Sobre a Livro Solto</Text>
+      <View style={styles.viewTexto}>
+        <Text style={styles.paragraph}>
+          A Livro Solto é uma iniciativa que uniu estudantes do Técnico de
+          Informática para Internet do Senac Penha na construção de um projeto
+          que acessibiliza os livros pela interação da comunidade leitora que
+          troca obras entre si.
+        </Text>
+        <Text style={styles.paragraph}>
+          Vemos a leitura como um agente de educação na sociedade. Portanto,
+          toda atitude, mesmo que pequena, é um grande impulso dessa causa.
+        </Text>
+        <Text style={styles.paragraph}>
+          Pessoas que lêem, se educam. E, pessoas educadas, ajudam a mudar a
+          realidade.
+        </Text>
+        <Text style={styles.paragraph}>
+          Caso queiram compartilhar ideias sobre como podemos melhorar a
+          interação do nosso site, entrem em contato pelo nosso email:
+        </Text>
+        <Text style={styles.email}>contato@livrosolto.com.br</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Sobre;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 16,
+    alignItems: "center",
+    fontFamily: "roboto",
+  },
+  titulo: {
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "roboto",
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  viewTexto: { marginVertical: 8 },
+  paragraph: {
+    textAlign: "justify",
+    marginVertical: 8,
+    fontSize: 18,
+    fontFamily: "roboto",
+  },
+  email: {
+    textAlign: "center",
+    fontSize: 18,
+    fontFamily: "roboto",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+});
